1044361610024efa50d4b7908510ed8c4ce38899
BrunoSola <brunosola3@gmail.com>
2024-01-25 20:50:30 -0300

    DTO pattern to return users

--- src/main/java/com/brunosola/springBootMongo/resources/UserResource.java
+++ src/main/java/com/brunosola/springBootMongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.brunosola.springBootMongo.resources;
 
 import com.brunosola.springBootMongo.domain.User;
+import com.brunosola.springBootMongo.dto.UserDTO;
 import com.brunosola.springBootMongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController //Identifica que a classe será um recurso REST
 @RequestMapping(value = "/users") // Caminho do endpoint da aplicação. http://localhost:8080/users
@@ -18,10 +20,13 @@ public class UserResource {
     @Autowired
     private UserService userService;
     @GetMapping //Idenfitica que o método será do tipo GET.
-    public ResponseEntity<List<User>> findAll(){ //ResponseEntity é uma classe Spring Web, utilizada para representar resposta HTTP(pode ser personalizado).
+    public ResponseEntity<List<UserDTO>> findAll(){ //ResponseEntity é uma classe Spring Web, utilizada para representar resposta HTTP(pode ser personalizado).
 
         //Busca no banco de dados e guarda na lista.
         List<User> users = userService.findAll();
-        return ResponseEntity.ok().body(users);
+        //convertendo User em UserDTO
+        //UserDTO::new= Esta é uma expressão lambda referindo-se a um construtor da classe 'UserDTO' que tem como argumento um 'User'.
+        List<UserDTO> userDTOS = users.stream().map(UserDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(userDTOS);
     }
 }
