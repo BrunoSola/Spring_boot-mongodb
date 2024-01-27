78c0ecc4ca7c0a2b07959a3d81da99018add1e83
BrunoSola <brunosola3@gmail.com>
2024-01-27 15:44:36 -0300

    Query with multiple criteria

--- src/main/java/com/brunosola/springBootMongo/resources/PostResource.java
+++ src/main/java/com/brunosola/springBootMongo/resources/PostResource.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController //Identifica que a classe será um recurso REST
@@ -25,6 +26,18 @@ public class PostResource {
     public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
         text = URL.decodeParam(text);
         List<Post> postList = postService.findByTitle(text);
+        return ResponseEntity.ok().body(postList);
+    }
+
+    @GetMapping(value = "/fullsearch")
+    public ResponseEntity<List<Post>> findByCriteria(
+            @RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "")String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "")String maxDate){
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date(0L));
+        List<Post> postList = postService.findByCriteria(text,min,max);
         return ResponseEntity.ok().body(postList);
     }
 }
--- src/main/java/com/brunosola/springBootMongo/services/PostService.java
+++ src/main/java/com/brunosola/springBootMongo/services/PostService.java
@@ -7,6 +7,7 @@ import com.brunosola.springBootMongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,5 +22,9 @@ public class PostService {
 
     public List<Post> findByTitle(String text){
         return postRepository.findByTitle(text);
+    }
+    public List<Post> findByCriteria(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000); // adicionando 24hrs em millisegundos por conta da comparação menor ou igual a maxDate na query.(hrs*min*seg*Milissegundos=hr em millisegundos)
+        return postRepository.findByCriteria(text, minDate, maxDate);
     }
 }
