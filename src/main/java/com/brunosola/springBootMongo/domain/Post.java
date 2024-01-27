8ca61925a115ceb028ed27c9509a90b2aa8fade3
BrunoSola <brunosola3@gmail.com>
2024-01-27 14:35:43 -0300

    Add comments to posts

--- src/main/java/com/brunosola/springBootMongo/domain/Post.java
+++ src/main/java/com/brunosola/springBootMongo/domain/Post.java
@@ -1,17 +1,21 @@
 package com.brunosola.springBootMongo.domain;
 
 import com.brunosola.springBootMongo.dto.AuthorDTO;
+import com.brunosola.springBootMongo.dto.CommentDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Document
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Getter @Setter @NoArgsConstructor
 public class Post implements Serializable {
 
     private String id;
@@ -19,6 +23,16 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
+
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
+        this.id = id;
+        this.date = date;
+        this.title = title;
+        this.body = body;
+        this.author = author;
+    }
 
     @Override
     public boolean equals(Object o) {
