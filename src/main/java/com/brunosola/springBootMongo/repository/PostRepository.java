2710163bd9ac0addb9fd9557621cfb4a573e0647
BrunoSola <brunosola3@gmail.com>
2024-01-27 15:06:24 -0300

    Simple query with @Query

--- src/main/java/com/brunosola/springBootMongo/repository/PostRepository.java
+++ src/main/java/com/brunosola/springBootMongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.brunosola.springBootMongo.repository;
 import com.brunosola.springBootMongo.domain.Post;
 import com.brunosola.springBootMongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,7 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }") //?0 = 1º parâmetro do método(no caso String text) - 'i' - ignore case sensitive //https://www.mongodb.com/docs/manual/reference/operator/query/regex/
+    List<Post> findByTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
