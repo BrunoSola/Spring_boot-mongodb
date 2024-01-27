779f35c09e4dc73d9d23c99609ca1a44c943a01d
BrunoSola <brunosola3@gmail.com>
2024-01-27 14:10:42 -0300

    Referencing posts with users

--- src/main/java/com/brunosola/springBootMongo/domain/User.java
+++ src/main/java/com/brunosola/springBootMongo/domain/User.java
@@ -1,22 +1,32 @@
 package com.brunosola.springBootMongo.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Document
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Getter @Setter @NoArgsConstructor
 public class User implements Serializable {
 
     @Id
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true) //Anotação para dizer que essa coleção é uma referência, e o parâmetro Lazy utilizado para não chamar todos os post sempre acessar um User, somente quando solicitado.
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 
     @Override
     public boolean equals(Object o) {
